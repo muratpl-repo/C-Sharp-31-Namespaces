f5d7a3c1b43b160b16abe48f136c94da6e269f16
muratpala007 <muratpala007@gmail.com>
2022-04-19 18:35:15 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _76DersNamespaces //Namespaces'lerin amaci classlari organize etmektir. Baska dosyalardan, olusturdugumuz namespacesleri 
+                            // cagirarak icerisindeki classlari kulanmamiza imkan verir. Ornek System namspaceini cagirarak string sinifini kulanabiliyoruz...
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
